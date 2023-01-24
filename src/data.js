b1dec225a6a6690077f3aaf11ec892c491abfebd
Bae Seong Whan <rlglxh@naver.com>
2023-01-24 23:24:59 +0900

    여행사 리액트

---
+++ src/data.js
@@ -0,0 +1,27 @@
+
+let data = [
+    {
+        id : 0,
+        title : "[모두의 Perfect ITALIA] 이탈리아/돌로미티/산마리노공국 품격일주 10일",
+        content : "해외 여행",
+        price : 2200000
+    },
+
+    {
+        id : 1,
+        title : "[얼리버드특가]1일자유/노쇼핑*싱가포르 5일/6일",
+        content : "해외 여행",
+        price : 199000
+    },
+
+    {
+        id : 2,
+        title : "하와이 항공 확보! 출발 확정!",
+        content : "해외 여행",
+        price : 2600000
+    }
+]
+
+
+
+export default data;
